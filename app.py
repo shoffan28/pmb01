49ad47786d50a17e308ae40bcc45d233a5a97752
shoffan28 <60082847+shoffan28@users.noreply.github.com>
2021-11-12 06:18:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template
-import pickle
 import pandas as pd
 import numpy as np
 from sklearn 
 import linear_model
+import pickle
 
 app = Flask(__name__)
 
