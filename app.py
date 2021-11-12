b74e27f8a3fe4b53e794e9de1948731d4dd49d59
shoffan28 <60082847+shoffan28@users.noreply.github.com>
2021-11-12 10:07:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def predict():
     if Metode=='Linear Regression':
         hasil =hasil
     else:
-        hasil=0.05*hasil
+        hasil=hasil + (0.05*hasil)
     return render_template('index.html', insurance_cost=int(abs(hasil)), Tahun=Tahun, Metode=Metode)
     '''
     Predict the insurance cost based on user inputs
